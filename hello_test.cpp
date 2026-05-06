74e30b51f6047b93b656bdf9a20dcd1d8f382283
agp <aagamanpokharel45@gmail.com>
2026-05-07 01:06:45 +0530

    versioned Test cases for vector class

--- hello_test.cpp
+++ hello_test.cpp
@@ -1,12 +1,37 @@
 //
 // Created by agp on 5/7/26.
 //
+#include <format>
 #include <gtest/gtest.h>
-
+#include "vector.h"
+using namespace std;
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+TEST(VectorTest, AddHappyPath) {
+    std::vector<double> a        = {1, 2, 3};
+    std::vector<double> b        = {4, 5, 6};
+    std::vector<double> expected = {5, 7, 9};
+    EXPECT_EQ(vector_add(a, b), expected);
+}
+TEST(VectorTest, SubtractHappyPath) {
+    std::vector<double> a        = {2, 3, 1};
+    std::vector<double> b        = {1, 2, 3};
+    std::vector<double> expected = {1, 1, -2};
+    EXPECT_EQ(vector_subtract(a, b), expected);
+}
+TEST(VectorTest, DotProduct) {
+    std::vector<double> a = {1, 2, 3};
+    std::vector<double> b = {4, 5, 6};
+    // 1*4 + 2*5 + 3*6 = 32
+    EXPECT_DOUBLE_EQ(vector_dot(a, b), 32.0);
+}
+TEST(VectorTest, AddShapeMismatch) {
+    std::vector<double> a = {1, 2, 3};
+    std::vector<double> b = {1, 2};
+    EXPECT_THROW(vector_add(a, b), std::invalid_argument);
+}
+TEST(VectorTest, AddEmptyVectors) {
+    std::vector<double> a = {};
+    std::vector<double> b = {};
+    std::vector<double> expected = {};
+    EXPECT_EQ(vector_add(a, b), expected);
 }
