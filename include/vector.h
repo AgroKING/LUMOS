d4552a1fd1fd947a9fe3a83fd021589af6308b2f
agp <aagamanpokharel45@gmail.com>
2026-05-06 22:53:28 +0530

    Vector implementation

--- include/vector.h
+++ include/vector.h
@@ -4,8 +4,14 @@
 
 #ifndef LUMOS_VECTOR_H
 #define LUMOS_VECTOR_H
-
-
+#include <vector>
+using namespace std;
+vector<double> vector_add(vector<double> a, vector<double> b);
+vector<double> vector_subtract(vector<double> a, vector<double> b);
+vector<double> vector_multiply(vector<double> a, vector<double> b);
+vector<double> vector_divide(vector<double> a, vector<double> b);
+double vector_dot(vector<double> a, vector<double> b);
+//vector<double> vector_cross(vector<double> a, vector<double> b);
 
 
 
--- src/vector.cpp
+++ src/vector.cpp
@@ -1,3 +1,68 @@
 //
 // Created by agp on 5/6/26.
 //
+#include "vector.h"
+
+vector<double> vector_add(vector<double> a, vector<double> b)
+{
+    vector<double> c(a);
+    if (a.size() == b.size())
+    {
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            c[i] = a[i] + b[i];
+        }
+    }
+    return c;
+}
+
+vector<double> vector_subtract(vector<double> a, vector<double> b)
+{
+    vector<double> c(a);
+    if (a.size() == b.size())
+    {
+        for (int i = 0; i < a.size(); i++)
+        {
+            c[i] = a[i] - b[i];
+        }
+    }
+    return c;
+}
+
+vector<double> vector_multiply(vector<double> a, vector<double> b)
+{
+    vector<double> c(a);
+    if (a.size() == b.size())
+    {
+        for (int i = 0; i < a.size(); i++)
+        {
+            c[i] = a[i] * b[i];
+        }
+    }
+    return c;
+}
+vector<double> vector_divide(vector<double> a, vector<double> b)
+{
+    vector<double> c(a);
+    if (a.size() == b.size())
+    {
+        for (int i = 0; i < a.size(); i++)
+        {
+            c[i] = a[i] / b[i];
+        }
+    }
+    return c;
+}
+
+double vector_dot(vector<double> a, vector<double> b)
+{    double c = 0.0;
+    if (a.size() == b.size())
+    {
+        for (int i = 0; i < a.size(); i++)
+        {
+            c += a[i] * b[i];
+        }
+    }
+    return c;
+}
